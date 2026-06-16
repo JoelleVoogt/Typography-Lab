98c527a47b978c760ab38dcafecb35ad3e683a9c
HeyImJoella <joelle.iris@gmail.com>
2026-06-16 17:15:48 +0200

    Finished fontsize styling slider

--- app/components/adjustments/weight.tsx
+++ app/components/adjustments/weight.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function Weight() {
   return (
     <>
@@ -6,7 +8,26 @@ export default function Weight() {
           <p className="w-full">Weight</p>
           <p className="w-full text-right">400</p>
         </div>
-        <input type="range" min={12} max={80} className="w-full" />
+
+        <div className="relative h-3">
+          <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 h-px bg-cream" />
+          <div className="absolute inset-0 flex justify-between pointer-events-none">
+            {[0, 1, 2, 3, 4].map((i) => (
+              <div key={i} className="w-px h-full bg-cream" />
+            ))}
+          </div>
+          <input
+            type="range"
+            min={100}
+            max={900}
+            className="weight-slider"
+            // value={weight}
+            // onChange={(e) => setWeight(Number(e.target.value))}
+            // aria-label={`Font weight, currently ${weight}`}
+          />
+        </div>
+
+        {/* <input type="range" min={12} max={80} className="w-full" /> */}
       </div>
     </>
   );
--- app/components/filtering/fontsize.tsx
+++ app/components/filtering/fontsize.tsx
@@ -6,7 +6,26 @@ export default function FontSize() {
       <div className="flex flex-row p-4 gap-2 items-center">
         <p className="">32px</p>
         <p className="text-lg font-bold">Aa</p>
-        <input type="range" min={12} max={80} />
+
+        <div className="flex flex-col p-4 gap-2 w-52">
+          <div className="relative h-3">
+            <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 h-px bg-cream" />
+            <div className="absolute inset-0 flex justify-between pointer-events-none">
+              {[0, 1, 2, 3, 4].map((i) => (
+                <div key={i} className="w-px h-full bg-cream" />
+              ))}
+            </div>
+            <input
+              type="range"
+              min={100}
+              max={900}
+              className="thumb-filter"
+              // value={weight}
+              // onChange={(e) => setWeight(Number(e.target.value))}
+              // aria-label={`Font weight, currently ${weight}`}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
