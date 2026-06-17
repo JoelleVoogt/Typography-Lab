3ae465f26febce86fd7056014e6504210c3bbee6
HeyImJoella <joelle.iris@gmail.com>
2026-06-17 13:49:34 +0200

    Added input field focus state and so on

--- app/components/filtering/typesomething.tsx
+++ app/components/filtering/typesomething.tsx
@@ -1,9 +1,11 @@
 export default function TypeSomething() {
   return (
-    <>
-      <div className="border-b border-b-[#54544D] xl:border-b-0 xl:border-r xl:border-r-[#54544D] flex flex-row p-4 gap-2 w-full">
-        <p>Type something...</p>
-      </div>
-    </>
+    <div className="border-b border-b-[#54544D] xl:border-b-0 xl:border-r xl:border-r-[#54544D] flex flex-row px-4 w-full focus-within:bg-[#51514b] transition-colors">
+      <input
+        type="text"
+        placeholder="Type something..."
+        className="bg-transparent outline-none w-full h-14 text-cream text-base font-medium placeholder:text-[#78776d] caret-accent"
+      />
+    </div>
   );
 }
